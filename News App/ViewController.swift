659414b651e3d6c00d08e4b97aed52b51a34e298
verianggoro <veryanggoro007@gmail.com>
2021-10-13 10:23:49 +0700

    Initial Commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Veri Anggoro on 13/10/21.
+//  Copyright © 2021 Veri Anggoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
